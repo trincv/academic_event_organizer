610a5a2465a3132f4b6dcd5aaa128242ed6c0ab0
Filipe Fiúza <163004155+trincv@users.noreply.github.com>
2025-06-03 20:46:49 -0300

    Update2 EventsManagment.java

--- src/main/java/br/academic/utils/events/EventsManagment.java
+++ src/main/java/br/academic/utils/events/EventsManagment.java
@@ -8,19 +8,13 @@ public class EventsManagment {
   
   public boolean addEvent(Object o) {
     if(!(o instanceof Events)) return 0;
-    
+    searchTitle.put(o.getTitle(), o);
   }
   
   public boolean removeEvent(Object o) {
     if(!(o instanceof Events)) return 0;
     
   }
-  
-  public void displayEvent(Object o) {
-    
-  
-  }
-
   public boolean searchEvent(Object o) {
     if(!(o instanceof Events)) return 0;
     
